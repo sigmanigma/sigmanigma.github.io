ded34236979ab1f74a4dd49c7383a488f68a6045
sigmanigma <bourqueno77@yahoo.com>
2018-11-07 00:17:23 -0800

    First upload

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+
+<html>
+<head>
+	<title>This is my PHP site</title>
+	<link type="text/css" rel="stylesheet" href="style/core.css" />
+</head>
+<body>
+	<header>
+		<h1>This is the header.</h1>
+		<nav>
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="index.php?projects">Projects</a></li>
+				<li><a href="index.php?about">About</a></li>
+				<li><a href="index.php?product">Product</a></li>
+				<li><a href="index.php?blog">Blog</a></li>
+				<li><a href="index.php?contact">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php'; ?>
+		
+	<div id="content">
+		<h3>Projects</h3>
+		<?php>
+			if(isset($_GET['about')){
+				echo 'this is about';
+			}
+		?>
+		
+	</div>
+	
+	<footer>
+	<p> This is footer </p>
+	</footer>
+	
+</body>
+
+</html>
